596599f8b5d03c0f416dc52e83d83bf2a821835c
Shaheer04 <sheeryjamal05@gmail.com>
2024-07-14 02:10:27 +0500

    added github workflow for batch predictions

--- Batch_inference_pipeline.py
+++ Batch_inference_pipeline.py
@@ -1,28 +1,31 @@
-LOCAL=False
 HOURS=24
-IMAGE_FOLDER="monitor"
 
 def g():
     import pandas as pd
     import hopsworks
     import joblib
     from datetime import datetime, timedelta
+    from dotenv import load_dotenv,find_dotenv
+    import os
+    from UI.load_model import model, preprocessing_pipeline
 
-    project = hopsworks.login(api_key_file='UI/featurestore.key', project='heartdisease')
+
+    load_dotenv(find_dotenv())
+    HOPSWORKS_KEY = os.getenv("API_KEY")
+    project = hopsworks.login(api_key_value=HOPSWORKS_KEY, project='heartdisease')
     fs = project.get_feature_store()
 
     mr = project.get_model_registry()
     model = mr.get_model("heart_model_v1", version=1)
-    model_dir = model.download()
-    print(f"{model_dir} I am here")
-    model = joblib.load("heart_model/heart_model.pkl")
-    preprocessing_pipeline = joblib.load("heart_model/preprocessing_pipeline.pkl")
+    
+    #model = joblib.load("heart_model/heart_model.pkl")
+    #preprocessing_pipeline = joblib.load("heart_model/preprocessing_pipeline.pkl")
     
     fg = fs.get_feature_group(name="heart_user_dataset", version=1)
     df = fg.read(read_options={"use_hive": True})
 
-    if df.shape[0] == 0:
-        print("No new data to predict")
+    if df.shape[0] <= 5:
+        print("No new data to predict right now!")
         return
     
     # Filter so we get only last data added
@@ -39,23 +42,29 @@ def g():
     df["timestamp"] = df['timestamp'] - pd.to_timedelta(0 * df.index, unit='s')
 
     y_true = df['heart_disease']
-    X = preprocessing_pipeline.transform(df)
-    y_pred = model.predict(X)
+    df = df.drop(columns=['heart_disease'])
+
+    try:
+        X = preprocessing_pipeline.transform(df)
+        y_pred = model.predict(X)
+    except:
+        print("Error in the model or preprocessing pipeline!")
+        return  
 
     # Store predictions and truth
     print("Storing predictions in Monitor_df")
     print(df)
     monitor_df = pd.DataFrame({"pred": y_pred, "true": y_true, "timestamp": df['timestamp']})
+    concat_df = pd.concat([df, monitor_df], axis=1)
     monitor_fg = fs.get_or_create_feature_group(
         name="heart_predictions",
         version=1,
-        primary_key=monitor_df.columns,
+        primary_key=concat_df.columns,
         description="Heart Monitoring Predictions",
         event_time="timestamp",
     )
     monitor_fg.insert(monitor_df, write_options={"wait_for_job": False})
 
-    print("Finished insertion of Monitor data!")
+    print("Finished Prediction on the user data!")
 
-if __name__ == "__main__":
-    g()
+g()
